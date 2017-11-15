088841241f96d26bbddcb28e506b1579b47ec1bb
Jackson Carter <jcarter0803@gmail.com>
2017-11-15 12:12:08 -0800

    RPN CALULATOR

--- reversePolish.js
+++ reversePolish.js
@@ -6,9 +6,35 @@
 //   ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
 //   ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
 
-var evalRPN = function(tokens) {
-
+function evalRPN(tokens) {
+    var stack = [];
+    
+    for(var i = 0; i < tokens.length; i++) {
+        var token = tokens[i];
+        var val1,val2;
+        var val = parseInt(token);
+        if(!isNaN(val)) {
+            stack.push(val);
+        } else {
+            val2 = stack.pop();
+            val1 = stack.pop();
+            
+            if(token === '*') {
+                stack.push(parseInt(val1 * val2));
+            } else if(token === '/') {
+                stack.push(parseInt(val1 / val2));
+            } else if(token === '-') {
+                stack.push(val1 - val2);
+            } else if(token === '+') {
+                stack.push(val1 + val2);
+            }
+        }
+    }
+    
+    return stack.pop();
 };
+   
 
 
-console.log(evalRPN([ 12, 12, 12, '*', '+', 3, 4, '-', '+' ] ));
+console.log(evalRPN(["2", "1", "+", "3", "*"]));
+console.log(evalRPN(["4", "13", "5", "/", "+"]));
